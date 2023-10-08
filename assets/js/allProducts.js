3a971c1688060c92a46cc066f7133465a8c8cb4f
Giovane Souza <developergiovanesouza@gmail.com>
2023-10-08 15:37:14 -0300

    Renderização de produtos de forma dinÂmica - ao checked do input no aside

--- assets/js/allProducts.js
+++ assets/js/allProducts.js
@@ -1,93 +1,210 @@
 const listProducts = document.querySelector('.products-list');
 
-const allProducts = []; // Guarda todos os produtos da API
+let allProducts = []; // Guarda todos os produtos da API
 
+// Pega todos os produtos
+const getAllProducts = () => {
 
-const loadProducts = () => {
+    const url = `https://fakestoreapi.com/products/`;
 
-    fetch('https://fakestoreapi.com/products')
-        .then(res => res.json())
+    return fetch(url)
+        .then(data => data.json())
         .then(products => {
-            // console.log(allProducts)
 
-            allProducts.push(products); // Salva os produtos da API (objetos) e deixa-os disponíveis p/ aplicação
+            allProducts.push(...products);  // Salva os produtos da API (objetos) e deixa-os disponíveis p/ aplicação (Usando spread para adicionar produtos individuais)
+            // return products;
+        })
+        .catch((e) => console.log(e))
+}
 
-            // Listagem dos produtos na página principal (index)
-            products.map((prod, key) => {
 
-                listProducts.innerHTML += `
-                
-                    <div class="product-card" key="${prod.id}">
+// Pega os produtos por categoria
+/*
+const getProductsByCategory = (category) => {
+
+    const url = `https://fakestoreapi.com/products/category/${category}`;
+
+    return fetch(url)
+        .then(data => data.json())
+        .then(products => {
             
-                    <div class="card-product-image">
-                        <img src="${prod.image}" alt="${prod.title}" />
-                        <i class="bi bi-suit-heart-fill" key="${key}"></i>
-                    </div>
+            return products;
+
+            // allProducts.push(...products); // Salva os produtos da API (objetos) e deixa-os disponíveis p/ aplicação (Usando spread para adicionar produtos individuais)
+
+        })
+        .catch(e => console.log(e))
+
+}
+
+*/
+
+const renderProducts = async (category) => {
+
+    listProducts.innerHTML = '';
+
+    let inputCategorias = document.querySelectorAll(`[data-category]`);
+
+    try {
+
+        console.log('aguardando requisição...')
+
+        // const categoria = await getProductsByCategory(category); // Aguarda o resultado da requisição e guarda as informações
+
+        const allProd = await getAllProducts(); // Aguarda o resultado da requisição e guarda as informações
+
+        // console.log(`O nome da categoria é: ${category}`)
+
+        inputCategorias.forEach(cat => {
+
+            let category;
+
+            cat.addEventListener('change', e => {
+
+                category = e.target.getAttribute('data-category'); // Pega o nome da categoria
+
+                let AllProd = document.querySelectorAll('div.product-card');
+                // let AllCategories = AllProd.getAttribute('data-category');
+
+
+                let isChecked = e.target.checked; // Verifica se está checado
+
+
+                // Muda o nome da categoria conforme input checked (Como o nome é em inglÊs, será inserido um em português)
+                let categoryName = category;
+                let nameCat;
+
+                switch (categoryName) {
+                    case `electronics`:
+                        nameCat = 'Eletrônicos';
+                        break;
+                    case `jewelery`:
+                        nameCat = 'Jóias';
+                        break;
+                    case `men's clothing`:
+                        nameCat = 'Roupas Masculinas';
+                        break;
+                    case `women's clothing`:
+                        nameCat = 'Roupas Femininas';
+                        break;
+                }
+
+
+                // Percorre os produtos para pegar o atributo data-category e comparar com o valor do data-category do input
+                for (let i = 0; i <= AllProd.length; i++) {
+
+                    // console.log(AllProd[i].getAttribute('data-category'))
+
+                    let categ = AllProd[i].getAttribute('data-category');
+
+
+                    if (categ !== category) {
+
+                        if (isChecked) {
+                            document.querySelector('h1').innerHTML = `${nameCat}`;
+                            AllProd[i].style.display = 'none'; // Todos os produtos que sejam != ao checked somem da tela
+                            console.log(isChecked);
+                        } else {
+                            document.querySelector('h1').innerHTML = `Nossos produtos`;
+                            AllProd[i].style.display = 'block';
+                        }
+
+
+                    }
+
+
+                }
+
+
+            });
+
+
+
+        });
+
+
+
+        // Listagem dos produtos na página principal (index)
+        allProducts.map((prod, key) => {
+
+            listProducts.innerHTML += `
             
-                    <div class="card-product-info">
-                    <!-- .slice(0, 16): Padroniza o tamanho dos títulos -->
-                        <h4 class="product-name">${prod.title.slice(0, 16)}</h4>
-            
-                        <div class="price-before">
-                            <span class="old-price">R$ ${(prod.price * 5).toFixed(2)}</span>
-                        </div>
-            
-                        <div class="price-now">
-                            <h5 class="new-price">R$ ${(prod.price * 3).toFixed(2)}</h5>
-                        </div>
-            
-                        <button class="add-bag" key="${key}">
-                            <i class="bi bi-bag-fill"></i>
-                            Adicionar à sacola
-                        </button>
-                    </div>
-            
+                <div class="product-card" key="${prod.id}" data-category="${prod.category}">
+        
+                <div class="card-product-image">
+                    <img src="${prod.image}" alt="${prod.title}" />
+                    <i class="bi bi-suit-heart-fill" key="${key}"></i>
                 </div>
-                
-                `;
+        
+                <div class="card-product-info">
+                <!-- .slice(0, 16): Padroniza o tamanho dos títulos -->
+                    <h4 class="product-name">${prod.title.slice(0, 16)}</h4>
+        
+                    <div class="price-before">
+                        <span class="old-price">R$ ${(prod.price * 5).toFixed(2)}</span>
+                    </div>
+        
+                    <div class="price-now">
+                        <h5 class="new-price">R$ ${(prod.price * 3).toFixed(2)}</h5>
+                    </div>
+        
+                    <button class="add-bag" key="${key}">
+                        <i class="bi bi-bag-fill"></i>
+                        Adicionar à sacola
+                    </button>
+                </div>
+        
+            </div>
+            
+            `;
+
+        });
 
 
-                // Adiciona os ítens à bag
-                const btnBuy = document.querySelectorAll('.add-bag');
+        // Adiciona os ítens à bag
+        const btnBuy = document.querySelectorAll('.add-bag');
 
-                btnBuy.forEach(item => {
+        btnBuy.forEach(item => {
 
-                    let key;
+            let key;
 
-                    item.addEventListener('click', e => {
+            item.addEventListener('click', e => {
 
-                        // Pega o id do produto
-                        key = e.target.getAttribute('key');
+                // Pega o id do produto
+                key = e.target.getAttribute('key');
 
-                        // Pega os produtos add à sacola -> salvos no localStorage
-                        const bagProducts = getBagProduct();
+                // Pega os produtos add à sacola -> salvos no localStorage
+                const bagProducts = getBagProduct();
 
-                        const addToBag = products[key]; // Pega os dados do produto a ser salvo
+                const addToBag = allProducts[key]; // Pega os dados do produto a ser salvo
 
-                        // console.log(addToBag)
+                // console.log(addToBag)
 
-                        bagProducts.push(addToBag); // Adiciona produto à lista
+                bagProducts.push(addToBag); // Adiciona produto à lista
 
-                        saveBagProducts(bagProducts); // atualiza lista no localStorage
+                saveBagProducts(bagProducts); // atualiza lista no localStorage
 
-                        // Exibe msg informando que houve a add do produto à sacola
-                        alert(`O item ${products[key].title} foi adicionado à sacola com sucesso!`);
+                // Exibe msg informando que houve a add do produto à sacola
+                alert(`O item ${allProducts[key].title} foi adicionado à sacola com sucesso!`);
 
-                        updateTotalItems();
-
-                    });
-
-                });
-
+                updateTotalItems();
 
             });
 
         });
 
 
+    } catch (e) {
+        console.log(e);
+    }
+
+
 }
 
-loadProducts();
+
+// Carrega todos os produtos na página
+renderProducts();
+
 
 
 // Pega todos os produtos salvos no localStorage, caso não exista retorna um array vazio
